27f76a53860c4ecf0acf14dd33b552053f2517c9
qweis <quirin.weis@gmail.com>
2016-06-06 00:25:20 -0700

    Final programm with added comments for clarity

--- ubidots_dht22.ino
+++ ubidots_dht22.ino
@@ -1,34 +1,32 @@
-#include "Adafruit_DHT/Adafruit_DHT.h"                                          //DHT libaries
-#include "HttpClient/HttpClient.h"                                              //Http libaries
-
+#include "Adafruit_DHT/Adafruit_DHT.h"                                          //DHT libaries found in the online particle ide
+#include "HttpClient/HttpClient.h"                                              //Http libaries found in the online particle ide
 
 HttpClient http;
-#define VARIABLE_ID  "ID"                                 //Variable ID (F) for Temperature inside DHT22
-#define VARIABLE_ID2 "ID"                                 //Variable ID (%) for Humidity inside DHT22
-#define VARIABLE_ID3 "ID"                                 //Variable ID(F) for Temperature outside LM335
-#define VARIABLE_ID4 "ID"                                 //Variable ID(F) for Temperature outside DHT22
-#define VARIABLE_ID5 "ID"                                 //Variable ID (%) for Humidity outside DHT22
 
-#define TOKEN "Token"                                     //Acess Token
+#define VARIABLE_ID  " "                                 //Variable ID for Temperature inside, must be set inbetween " "
+#define VARIABLE_ID2 " "                                 //Variable ID for Humidity inside, must be set inbetween " "                                         
+#define VARIABLE_ID4 " "                                 //Variable ID for Temperature outside, must be set inbetween " "           
+#define VARIABLE_ID5 " "                                 //Variable ID for Humidity outside, must be set inbetween " "
 
-#define DHTPINinside 2                                    // what digital pin DHT22 inside is connected to
-#define DHTPINoutside 3                                   // what digital pin DHT22 outside is connected to
+#define TOKEN " "                                        //Acess Token, must be set inbetween " "
 
-#define DHTinsideTYPE DHT22		                            // DHT 22 
-#define DHToutsideTYPE DHT22	                            // DHT 22 
+#define DHTPINinside 2                                   //Photon Pin used for DHT sensor inside
+#define DHTPINoutside 3                                  //Photon Pin used for DHT sensor outside
 
-unsigned long lastTime = 0UL;
-float temp_in_kelvin=0, temp_in_fahrenheit=0;
+#define DHTinsideTYPE DHT22                              //Define DHT type (either DHT22 or DHT11)
+#define DHToutsideTYPE DHT22  
+
+unsigned long lastTime = 0UL;                            //Define lastTime to 0
 
 
 DHT dhtinside(DHTPINinside, DHTinsideTYPE);
 DHT dhtoutside(DHTPINoutside, DHToutsideTYPE);
 
 
-http_header_t headers[] = {
+http_header_t headers[] = {                             //https://github.com/nmattisson/HttpClient                   
       { "Content-Type", "application/json" },
       { "X-Auth-Token" , TOKEN },
-    { NULL, NULL }                                        // NOTE: Always terminate headers will NULL
+    { NULL, NULL }                                      //NOTE: Always terminate headers will NULL
 };
 
 http_request_t request;
@@ -37,43 +35,28 @@ http_response_t response;
 
 void setup() {
     
-    request.hostname = "things.ubidots.com";
+    request.hostname = "things.ubidots.com";            //From Ubidots http://ubidots.com/docs/api/
     request.port = 80;
     
-    dhtinside.begin();
+    dhtinside.begin();                                  //Begin reading form the DHT sensor
     dhtoutside.begin();
     
-    RGB.control(true);                                    // Set Particle Core Status LED to low brightness
+    RGB.control(true);                                  //Set the Photon rgb led to a lower brightness 
     RGB.brightness(3);
     RGB.control(false);
 }
 
-void loop() {
+void loop() {                                           //Start the Loop to post the Temperature to the Server 
     
+    unsigned long now = millis();
     
-    unsigned long now = millis();                         //Every 10 min publish uptime
-    
-    if (now-lastTime>300000UL) {
-        lastTime = now;
+     
+    if (now-lastTime>300000UL) {                        //Check if time has progressed for 5 min if so post Temperature to the Server
+        lastTime = now;                                 //300000 milliseconds = 5 minutes, 600000 msec = 10 min
         
-        float h_inside = dhtinside.getHumidity();
-   	    float t_inside = dhtinside.getTempCelcius();
-   	    float f_inside = dhtinside.getTempFarenheit();
-   	    
-   	    float h_outside = dhtoutside.getHumidity();
-   	    float t_outside = dhtoutside.getTempCelcius();
-   	    float f_outside = dhtoutside.getTempFarenheit();
-   	    
-   	    temp_in_kelvin = analogRead(0) * 0.08056640625;
-   	    temp_in_fahrenheit = ((temp_in_kelvin ) * 9 / 5) - 460.67;          // Calculate LM335 temperature 
-   	    
-        request.path = "/api/v1.6/variables/"VARIABLE_ID"/values";
-        request.body = "{\"value\":" + String(f_inside) + "}";
-        http.post(request, response, headers);
-        
-        request.path = "/api/v1.6/variables/"VARIABLE_ID2"/values";
-        request.body = "{\"value\":" + String(h_inside) + "}";
-        http.post(request, response, headers);
+        float h_outside = dhtoutside.getHumidity();
+        float t_outside = dhtoutside.getTempCelcius();
+        float f_outside = dhtoutside.getTempFarenheit();
         
         request.path = "/api/v1.6/variables/"VARIABLE_ID4"/values";
         request.body = "{\"value\":" + String(f_outside) + "}";
@@ -83,9 +66,16 @@ void loop() {
         request.body = "{\"value\":" + String(h_outside) + "}";
         http.post(request, response, headers);
         
-        request.path = "/api/v1.6/variables/"VARIABLE_ID3"/values";
-        request.body = "{\"value\":" + String(temp_in_fahrenheit) + "}";
+        float h_inside = dhtinside.getHumidity();
+        float t_inside = dhtinside.getTempCelcius();
+        float f_inside = dhtinside.getTempFarenheit();
+        
+        request.path = "/api/v1.6/variables/"VARIABLE_ID"/values";
+        request.body = "{\"value\":" + String(f_inside) + "}";
         http.post(request, response, headers);
-
+        
+        request.path = "/api/v1.6/variables/"VARIABLE_ID2"/values";
+        request.body = "{\"value\":" + String(h_inside) + "}";
+        http.post(request, response, headers);
     }
 }
